06e481b4d80505e1969bacf8d5d170d086aae13c
JΛVI <hello@soyjavi.com>
2021-04-30 18:17:59 +0700

    Store channel messages

--- src/repositories/Telegram/Telegram.js
+++ src/repositories/Telegram/Telegram.js
@@ -1,4 +1,7 @@
 const { Telegram: Telegraf } = require('telegraf');
+import { Storage } from 'vanilla-storage';
+
+import { C } from '@commons';
 
 const { TELEGRAM_BOT, TELEGRAM_CHANNEL_MASTER } = process.env;
 
@@ -11,8 +14,10 @@ export const Telegram = {
         parse_mode: 'Markdown',
         disable_web_page_preview: true,
       })
-      .then((response) => {
-        console.log({ response });
+      .then(({ message_id: id, date: timestamp, text }) => {
+        const store = new Storage(C.STORE.MESSAGES);
+
+        store.push({ id, text, timestamp });
       })
       .catch((error) => console.log({ error }));
   },
